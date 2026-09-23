4a99d6f3bac7c5609f9e8ff87176b2852cf077fa
tamiressg <t.graziele@escolar.ifrn.edu.br>
2026-09-23 12:02:37 -0300

    Delete Crud-Professores-main directory

--- Crud-Professores-main/README.md
+++
@@ -1,2 +0,0 @@
-<img width="1920" height="971" alt="image" src="https://github.com/user-attachments/assets/646ebf46-2e8f-496a-a153-6cd378e5b388" />
-
--- Crud-Professores-main/src/components/MensagemErro.jsx
+++
@@ -1,7 +0,0 @@
-function MensagemErro({ mensagem }) {
-  if (!mensagem) return null
-
-  return <p className="mensagem-erro">{mensagem}</p>
-}
-
-export default MensagemErro
